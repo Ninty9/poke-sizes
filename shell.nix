990acb26f5726a28c48be7fe2f3ba21860a724ba
Ninty <79477736+Ninty9@users.noreply.github.com>
2024-12-21 22:51:12 +0100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,28 +1,9 @@
-/*
-based on
-https://discourse.nixos.org/t/how-can-i-set-up-my-rust-programming-environment/4501/9
-*/
-let
-  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
-  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-  rustVersion = "latest";
-  #rustVersion = "1.62.0";
-  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-    extensions = [
-      "rust-src" # for rust-analyzer
-      "rust-analyzer"
-    ];
-  };
-in
+{ pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  buildInputs = [
-    rust
-  ] ++ (with pkgs; [
-    openssl_3
-    pkg-config
-    # other dependencies
-    #gtk3
-    #wrapGAppsHook
-  ]);
-  RUST_BACKTRACE = 1;
+  nativeBuildInputs = with pkgs; [ rustc cargo gcc rustfmt clippy pkg-config ];
+  buildInputs = with pkgs; [ openssl ];
+  # Certain Rust tools won't work without this
+  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
